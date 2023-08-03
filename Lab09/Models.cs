540708184c4146a3edf81dd57da003488ddb96d1
starlit-nox <kdkelly.1998@gmail.com>
2023-08-02 21:25:02 -0500

    Added Models
    Added the two main classes
    updated the models section
    app not working yet

---
+++ Lab09/Models.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab09
+{
+    public class Property
+    {
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Address { get; set; }
+        public string Borough { get; set; }
+        public string Neighborhood { get; set; }
+        public string County { get; set; }
+    }
+    public class Feature
+    {
+        public string Type { get; set; }
+        public Property Properties { get; set; }
+    }
+}
---
+++ Lab09/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // this will read the json data
+            string jsonFilePath = "data.json";
+            string jsonData = File.ReadAllText(jsonFilePath);
+
+            // deserializes json into featurecollection
+            FeatureCollection featureCollection = JsonConvert.DeserializeObject<FeatureCollection>(jsonData);
+        }
+        public class FeatureCollection
+        {
+            public string Type { get; set; }
+            public List<Feature> Features { get; set; }
+        }
+    }
+}
